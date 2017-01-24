919c91110192dfa558082118919efcb7cf6dc3b2
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2017-01-24 17:19:41 +0200

    respond with a message to queries

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('Hello World Edu Bot');
+});
+
+router.get('/unknown/', (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    messages: [
+      {text: 'Unknown! sorry! => ' + JSON.stringify(req.query)}
+    ]
+  }));
 });
 
 module.exports = router;
