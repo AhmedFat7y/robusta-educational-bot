1e3f12cb8e1adbe5ffde47fc3dbac2a487bd1da6
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2017-01-24 17:29:23 +0200

    linked it to apiai

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 let express = require('express');
+let apiai = require('apiai');
 let router = express.Router();
-
+let apiAiService = apiai('4869a2ed9f3f4374b2eb5a2af2663d33', {language: 'en', requestSource: 'fb'});
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.send('Hello World Edu Bot');
@@ -8,11 +9,26 @@ router.get('/', function (req, res, next) {
 
 router.get('/unknown/', (req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
-    messages: [
-      {text: 'Unknown! sorry! => ' + JSON.stringify(req.query)}
-    ]
-  }));
+  apiAiService.textRequest(req.query.q, {
+    sessionId: req.query.fbId
+  }, (err, apiaiRes) => {
+    let message;
+    if (err) {
+      console.error('Error communicating with apiai', err);
+      message = {
+        messages: [
+          {text: 'Error Happened! sorry!!!'}
+        ]
+      }
+    } else {
+      message = {
+        messages: [
+          {text: req.query.q + ' => ' + apiaiRes.result.fulfillment.speech}
+        ]
+      }
+    }
+    res.send(JSON.stringify(message));
+  });
 });
 
 module.exports = router;
